919e123493235eed93d3e0494834696e4cde51c9
larrydaguilera <larrydaguilera@gmail.com>
2022-06-07 23:25:29 -0300

    Entities Created

--- src/main/java/com/alkemy/icons/icons/entity/ContinenteEntity.java
+++ src/main/java/com/alkemy/icons/icons/entity/ContinenteEntity.java
@@ -1,4 +1,22 @@
 package com.alkemy.icons.icons.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "continente")
+@Getter
+@Setter
 public class ContinenteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String imagen;
+
+    private String denominacion;
 }
